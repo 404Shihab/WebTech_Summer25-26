b2af282037ff0abceaf695f5d1ddd28d5aad2b8f
404Shihab <shihabbhuiyan500@gmail.com>
2026-07-08 22:10:11 +0600

    Enhance student registration form with improved styling and validation script

---
+++ web tech lab/html css js/JS/script.js
@@ -0,0 +1,19 @@
+console.log("file connected")
+
+
+function collect_data(){
+    let fname = document.getElementById("fname").value;
+    console.log(fname);
+
+    let email = document.getElementById("email").value;
+    console.log(email);
+
+    let phone = document.getElementById("phone").value;
+    console.log(phone);
+
+
+    let dob = document.getElementById("dob").value;
+    console.log(dob);
+
+    return false;
+}
