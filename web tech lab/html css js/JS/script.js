6b222c88f0ab1f50af835fad3d359b57670e1819
404Shihab <shihabbhuiyan500@gmail.com>
2026-07-09 09:57:09 +0600

    Enhance data collection function by adding fields for gender, student ID, department, semester, address, and profile

--- web tech lab/html css js/JS/script.js
+++ web tech lab/html css js/JS/script.js
@@ -15,5 +15,27 @@ function collect_data(){
     let dob = document.getElementById("dob").value;
     console.log(dob);
 
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    console.log(gender);
+
+    let studentID = document.getElementById("studentID").value;
+    console.log(studentID);
+
+    let dept = document.getElementById("department").value;
+    console.log(dept);
+
+    let semester = document.getElementById("semester").value;
+    console.log(semester);
+
+    // let hobbies =document.querySelector('input[name="hobbies"]:checked').value;
+    // console.log(hobbies);
+
+    let address = document.getElementById("address").value;
+    console.log(address);
+
+    let profile =document.getElementById("profile").value;
+    console.log(profile);
+
+
     return false;
 }
